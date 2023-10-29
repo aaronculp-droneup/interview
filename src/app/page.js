5c32045bd2844f38792264fed658a4377d0cb11b
Aaron Culp <aaron.culp@droneup.com>
2023-10-29 15:51:01 -0400

    saving final updates

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,53 @@
+'use client'
+
+// Libraries
+
 import Image from 'next/image'
+import { useCallback, useState } from 'react'
+
+// Dependencies 
+
 import styles from './page.module.css'
+import Browns from '../../public/browns.jpeg';
+import Cavs from '../../public/cavs.png';
+import Guardians from '../../public/guardians.jpg';
+
+// Public
 
 export default function Home() {
+  const [imageSet] = useState([Browns, Cavs, Guardians]);
+  const [currentImg, setCurrentImg] = useState(0);
+
+  const changeImg = useCallback((e) => {
+    const { id } = e.currentTarget
+
+    if (id === 'previous') {
+      setCurrentImg((prevIndex) =>
+        prevIndex - 1 < 0 ? imageSet.length - 1 : prevIndex - 1)
+    } else {
+      setCurrentImg((prevIndex) =>
+        prevIndex + 1 === imageSet.length ? 0 : prevIndex + 1)
+    }
+
+  }, [currentImg])
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
+      <button id="previous" onClick={(e) => changeImg(e)}>
+        Previous Image
+      </button>
       <div className={styles.center}>
         <Image
           className={styles.logo}
-          src="/next.svg"
+          src={imageSet[currentImg]}
           alt="Next.js Logo"
           width={180}
           height={37}
-          priority
         />
       </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <button id="next" onClick={(e) => changeImg(e)}>
+        Next Image
+      </button>
     </main>
   )
 }
